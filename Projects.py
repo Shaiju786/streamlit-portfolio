27a606c7efea43773c8eceec9341cf5159077025
Shaiju786 <shaijushajahan92@gmail.com>
2025-06-29 10:24:58 +0400

    Add files via upload

---
+++ Projects.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("💻 Projects")
+
+st.header("1. Expense Tracker (Streamlit)")
+st.markdown("""
+A beginner-friendly app to manage personal expenses.  
+**Tech Stack:** Python, Streamlit, Pandas  
+**Code:** [GitHub Repo](https://github.com/Shaiju786/expense-tracker-streamlit)
+""")
