0e02110e46ff1db3598245ede2ae839a281418d1
Shaiju786 <shaijushajahan92@gmail.com>
2025-06-29 11:31:46 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,22 +1,20 @@
 import streamlit as st
+from projects import show_projects  # 👈 Add this line
 
 st.set_page_config(page_title="Shaiju's Portfolio", layout="wide")
 
-# Sidebar menu
 st.sidebar.title("Navigate")
 selection = st.sidebar.radio("Go to", ["Home", "Projects", "Blogs", "Contact"])
 
-# Display corresponding page
 if selection == "Home":
     st.title("Hi, I'm Shaiju!")
     st.write("Welcome to my portfolio. Use the sidebar to explore.")
 elif selection == "Projects":
-    st.subheader("Projects")
-    st.write("List of projects will appear here...")
+    show_projects()  # 👈 Update this
 elif selection == "Blogs":
     st.subheader("Blogs")
     st.write("Blog content goes here...")
 elif selection == "Contact":
     st.subheader("Contact")
     st.write("You can reach me at shaiju@example.com")
-    
+
