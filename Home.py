4ce84d1928e223cbaeb7815022c8ad504604beb9
Shaiju786 <shaijushajahan92@gmail.com>
2025-06-29 10:00:14 +0400

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Shaiju's Portfolio", page_icon="💼", layout="centered")
-
 st.title("👋 Hi, I'm Shaiju!")
-st.markdown("Welcome to my portfolio. Use the sidebar to explore my Projects, Blogs, or Contact.")
+st.write("Welcome to my portfolio. Use the sidebar to explore.")
