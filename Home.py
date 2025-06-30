98378243fa454707d87bba2b0a4b474b71a191fc
Shaiju786 <shaijushajahan92@gmail.com>
2025-06-30 09:53:13 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -43,13 +43,13 @@ elif selection == "Projects":
 elif selection == "Blogs":
     st.title("📚 Blogs")
 
-    st.write("🚧🗓️ First Project Reflection: Expense Tracker
+    st.write("""🚧🗓️ First Project Reflection: Expense Tracker
 
 What started as a simple terminal-based script using `input()` turned into a working web app! 🚀  
 Key takeaways:
 - Streamlit helps turn Python scripts into real apps — fast
 - GitHub and deployment may seem hard at first, but it's all just steps
-- Always name your `requirements.txt` properly 😅")
+- Always name your `requirements.txt` properly 😅""")
 
 # ------------------------
 # Section: Contact
